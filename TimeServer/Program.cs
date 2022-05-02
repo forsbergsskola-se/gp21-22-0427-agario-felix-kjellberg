be1679eaa07ab59a5a97838582cf4daccc42c7c6
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-05-02 13:02:00 +0200

    Should now stop listening after client connected. And close the stream when message is sent.

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -12,6 +12,10 @@ tcpListener.Start();
 
 var tcpClient = tcpListener.AcceptTcpClient();
 
+tcpListener.Stop();
+
 byte[] buffer = new byte[100];
 buffer = Encoding.ASCII.GetBytes(DateTime.Now.ToString());
 if (tcpClient != null) tcpClient.GetStream().Write(buffer);
+tcpClient.GetStream().Close();
+tcpClient.Close();
