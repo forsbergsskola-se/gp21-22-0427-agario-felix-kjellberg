43fd20c1269b778b5ffda80cf2f903578ad8a4bc
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-05-03 15:47:59 +0200

    Added Thread to TimeServer

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 var endpoint = new IPEndPoint(
     IPAddress.Loopback,
@@ -9,13 +10,13 @@ var endpoint = new IPEndPoint(
 TcpListener tcpListener = new (endpoint);
 tcpListener.Start();
 
-
 var tcpClient = tcpListener.AcceptTcpClient();
+new Thread(() => {
+    var stream = tcpClient.GetStream();
 
-tcpListener.Stop();
-
-byte[] buffer = new byte[100];
-buffer = Encoding.ASCII.GetBytes(DateTime.Now.ToString());
-if (tcpClient != null) tcpClient.GetStream().Write(buffer);
-tcpClient.GetStream().Close();
-tcpClient.Close();
+    byte[] buffer = new byte[100];
+    buffer = Encoding.ASCII.GetBytes(DateTime.Now.ToString());
+    stream.Write(buffer);
+    stream.Close();
+    tcpClient.Dispose();
+}).Start();
