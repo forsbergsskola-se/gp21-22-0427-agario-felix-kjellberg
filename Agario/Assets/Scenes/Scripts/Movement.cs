6c3242f1879f58e8eafafa129d296ba5659eb8be
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-05-20 16:01:04 +0200

    Created Circle class, Doesnt work as intended, but should be able to update size

--- Agario/Assets/Scenes/Scripts/Movement.cs
+++ Agario/Assets/Scenes/Scripts/Movement.cs
@@ -11,7 +11,6 @@ public class Movement : MonoBehaviour{
 
     void Update(){
         if (Camera.main is not null) mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(mousePosition);
         //TODO: Movement seems somewhat staggering
         var mouseDirection = mousePosition - (Vector2)transform.position;
         rigidbody2D.MovePosition((Vector2)this.transform.position + mouseDirection.normalized * Time.deltaTime * moveSpeed);
